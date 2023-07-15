1af9579c54b2af09fb3180194f6e643d1aed0c8d
Jonathan Urzola <jonathan@dekosas.com>
2023-07-15 14:51:22 -0500

    First commit

---
+++ i_pay/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from i_pay.users.models import User
+from i_pay.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
